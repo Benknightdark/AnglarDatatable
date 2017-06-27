43e195d20f3bd44271b93c89e21992fc96793653
Kung Ben <Kung Ben>
2017-06-27 11:28:48 +0800

    ee

--- src/app/customtable/customtable.component.ts
+++ src/app/customtable/customtable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import { UserdataService } from '../services/userdata.service';
 
@@ -10,26 +10,40 @@ import { UserdataService } from '../services/userdata.service';
 })
 export class CustomtableComponent implements OnInit {
   tabledata
-  TableSetting: any={};
+  TableSetting: any = {};
   Columns = [];
-  Pages=[];
-  constructor(private http: UserdataService) {}
+  Pages = [];
+  ActivePage;
+  constructor(private http: UserdataService) { }
 
   ngOnInit() {
-   this.TableSetting={start:0,length:10};
+    this.TableSetting = { start: 0, length: 10 };
     this.Columns = ['ID', 'UserName', "DisplayName", "UserType", "IsEnabled", "IsAdmin", "Email", "Tel", "Mobile", "Remark", "ExpiredDT", "LastLogonDT", "InitUID", "InitDT", "ModifiedUID", "ModifiedDT"];
     this.http.GetUserData(this.TableSetting).subscribe(a => {
       this.tabledata = (a.data)
       console.log(a)
-      console.log(Math.ceil(14/10))
-      for(let i=0;i<Math.ceil(14/10);i++){
-        this.Pages.push(i+1)
+      console.log(Math.ceil(14 / 10))
+      for (let i = 0; i < Math.ceil(a.totoalcount / a.length); i++) {
+        this.Pages.push(i + 1)
       }
+      this.ActivePage = 1;
     })
     //  this.tabledata
   }
-  Detail(id){console.log("detail",id)}
-  Edit(id){console.log("Edit",id)}
-  Delete(id){console.log("Delete",id)}
+  Detail(id) { console.log("detail", id) }
+  Edit(id) { console.log("Edit", id) }
+  Delete(id) { console.log("Delete", id) }
+  next() {
+    if (this.ActivePage !== this.Pages[this.Pages.length - 1]) {
+      this.ActivePage = this.ActivePage + 1
+    }
+
+  }
+  previous() {
+    if (this.ActivePage !== 1) {
+      this.ActivePage = this.ActivePage - 1
+    }
+
+  }
 
 }
